2a6785144dcc6d09b758fc28c3f3b6b2b1a5f12b
nicolaasnel <nicolaas.cm.nel@gmail.com>
2014-06-11 15:09:45 +0200

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+def validate_contains_header(page_name, page_pos, text="")
+	describe page_name
+		it "#{page_name} should have the content '#{text}'" do
+      		visit page_pos
+      		expect(page).to have_content(text)
+    	end
+    end
+
+def validate_contains_title(page_name, page_pos)
+	describe page_name
+
+		it "#{page_name} page should have the right title" do
+      		visit page_pos
+      		expect(page).to have_title("Ruby on Rails Tutorial Sample App | #{page_name}")
+    	end
+end
+ 
+describe "StaticPages" do
+
+  	validate_contains_header("Home page","/static_pages/home", "Sample App")
+  	validate_contains_header("Help page","/static_pages/help", "Help")
+	validate_contains_header("About page","/static_pages/about", "About Us")
+
+  	validate_contains_title("Home","/static_pages/home")
+  	validate_contains_title("Help","/static_pages/help")
+	validate_contains_title("About Us","/static_pages/about")
+
+end
