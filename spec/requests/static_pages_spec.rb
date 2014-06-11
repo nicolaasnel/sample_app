9ae3ec53e4de4ea8db597fd190c07bb1f5ff4244
nicolaasnel <nicolaas.cm.nel@gmail.com>
2014-06-11 15:36:49 +0200

    Add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,10 +21,12 @@ describe "StaticPages" do
 
   	validate_contains_header("Home page","/static_pages/home", "Sample App")
   	validate_contains_header("Help page","/static_pages/help", "Help")
-	validate_contains_header("About page","/static_pages/about", "About Us")
+	  validate_contains_header("About page","/static_pages/about", "About Us")    
+    validate_contains_header("Contact page","/static_pages/contact", "Contact Us")
 
   	validate_contains_title("Home","/static_pages/home")
-  	validate_contains_title("Help","/static_pages/help")
-	validate_contains_title("About Us","/static_pages/about")
+  	validate_contains_title("Help","/static_pages/help")    
+    validate_contains_title("About","/static_pages/about")
+	  validate_contains_title("Contact","/static_pages/contact")
 
 end
